7a4d3a3d5bb66be761f93649035f9a727a1c5f66
Brian Rosengarten <barosengarten@gmail.com>
2019-01-03 04:47:57 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,13 +1,13 @@
 class Song
 
-  attr_accessor :name, :artist
+  attr_accessor :name
+  attr_reader :artist
 
   @@all = []
 
   def initialize(name, artist = nil)
     @name = name
-#    @artist = artist
-    self.artist=(artist)
+    @artist = artist
   end
 
   def self.all
