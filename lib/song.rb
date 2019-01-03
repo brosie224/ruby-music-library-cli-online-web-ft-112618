31f2f560657215f627d22a1918019c14ef16675c
Brian Rosengarten <barosengarten@gmail.com>
2019-01-03 05:20:58 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -8,7 +8,7 @@ class Song
   def initialize(name, artist = nil, genre = nil)
     @name = name
     self.artist= artist if artist != nil
-    @genre = genre
+    self.genre= genre
   end
 
   def self.all
